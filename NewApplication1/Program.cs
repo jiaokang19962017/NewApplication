bd85ca070fa0c9f1775c0acd38a4f259313054d3
焦康 <jiaokang@HP201-1>
2017-05-09 16:20:04 +0800

    更新

--- NewApplication1/Program.cs
+++ NewApplication1/Program.cs
@@ -8,15 +8,15 @@ namespace NewApplication1
     {
         static void Main(string[] args)//应用程序的一个入口:main方法,方法名建议首字母大写
         {
-            //Console.WriteLine();//快捷键:cw+2次tab键
-            //Console.WriteLine("hello world");//屏幕输出
-            //Console.ReadLine();//输入
-            //Console.WriteLine("我叫{0},今年{1}", "焦康", 21);  //占位符 
-            ////string name = Console.ReadLine();
-            ////string age = Console.ReadLine();
-            ////Console.WriteLine("我的名字是:"+name+",我的年龄是:"+age+"岁");  //
-            //Console.WriteLine("这个值是:{0:c3}",500);//数字格式化,c后面是几就保留几位小数,对于货币单位来说
-            //Console.ReadLine();//输入
+            Console.WriteLine();//快捷键:cw+2次tab键
+            Console.WriteLine("hello world");//屏幕输出
+            Console.ReadLine();//输入
+            Console.WriteLine("我叫{0},今年{1}", "焦康", 21);  //占位符 
+            string name = Console.ReadLine();
+            string age = Console.ReadLine();
+            Console.WriteLine("我的名字是:" + name + ",我的年龄是:" + age + "岁");  
+            Console.WriteLine("这个值是:{0:c3}", 500);//数字格式化,c后面是几就保留几位小数,对于货币单位来说
+            Console.ReadLine();//输入
             Console.WriteLine("指定宽度:|{0,10}|",500);//指定宽度
             Console.WriteLine("货币:{0:c3}",500);
             Console.WriteLine("十进制:{0:d3}", 50);//十进制
