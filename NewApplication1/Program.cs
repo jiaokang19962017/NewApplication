38067b11e930e8cea753e207b7916cc6ed1a18d9
焦康 <jiaokang@HP201-1>
2017-05-09 15:36:28 +0800

    字符串拼接

--- NewApplication1/Program.cs
+++ NewApplication1/Program.cs
@@ -11,7 +11,12 @@ namespace NewApplication1
             Console.WriteLine();//快捷键:cw+2次tab键
             Console.WriteLine("hello world");//屏幕输出
             Console.ReadLine();//输入
-            Console.WriteLine("我叫{0},今年{1}","焦康",21);  //占位符 
+            Console.WriteLine("我叫{0},今年{1}", "焦康", 21);  //占位符 
+            string name = Console.ReadLine();
+            string age = Console.ReadLine();
+            Console.WriteLine("我的名字是:"+name+",我的年龄是:"+age+"岁");  //嗯
+
+
         }
     }
 
